e74ea9090720acba1dc0388fa884f6b84b72b729
sandguine <stanwisuth@gmail.com>
2020-05-26 15:19:25 -0700

    add tasks structure and import jspsych plugins? '

---
+++ js/var.js
@@ -0,0 +1,7 @@
+// variable for the screen
+var canvas = document.getElementById("myCanvas");
+var left = canvas.getContext("2d");
+var right = canvas.getContext("2d");
+var veil = canvas.getContext("2d");
+var midline = canvas.getContext("2d");
+var angle;
