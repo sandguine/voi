fffb2581cffe17dfe1eddeaa510866a39c087c19
Sandy Tanwisuth <stanwisuth@gmail.com>
2020-05-31 23:27:40 -0700

    test functions variable

--- js/functions.js
+++ js/functions.js
@@ -25,7 +25,7 @@ function drawCircle(x, y, r, color_left, color_right){
 // draw marks inputs: center at x coordinate, y coordinate, and radius of a circle
 function drawMarks(x, y, r){
     for (var i = 0; i < 12; i++) {
-        angle = (i - 3) * (Math.PI * 2) / 12;       // THE ANGLE TO MARK.
+        angle = (i - 3) * (2 * Math.PI) / 12;       // THE ANGLE TO MARK.
         ctx.lineWidth = 5;            // HAND WIDTH.
         ctx.beginPath();
 
@@ -40,4 +40,39 @@ function drawMarks(x, y, r){
         ctx.strokeStyle = 'White';
         ctx.stroke();
     }
+}
+
+
+// draw marks inputs: center at x coordinate, y coordinate, and radius of a circle
+function drawLine(x, y, r, a){
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'Crimson';
+    ctx.beginPath();
+
+    a1 = Math.ceil(a/30) * (2 * Math.PI) / 12;
+
+    var x1 = (x) + Math.cos(a1) * (r);
+    var y1 = (y) + Math.sin(a1) * (r);
+
+    a2 = Math.ceil((a/30)+6) * (2 * Math.PI) / 12;
+
+    var x2 = (x) + Math.cos(a2) * (r);
+    var y2 = (y) + Math.sin(a2) * (r);
+
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x, y);
+    ctx.moveTo(x2, y2);
+    ctx.lineTo(x, y);
+
+    ctx.stroke();
+}
+
+
+function drawDot(x, y, r, a){
+    ctx.fillStyle = "SpringGreen";
+
+    var x1 = (x) + Math.cos(a) * (r);
+    var y1 = (y) + Math.sin(a) * (r);
+
+    ctx.fillRect(x1, y1, 10, 10);
 }
